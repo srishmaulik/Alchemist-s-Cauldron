372b649a17bccfc9d083efc6ce00ee27a01da6dd
srishmaulik <srishmaulik2004@gmail.com>
2024-04-10 12:59:15 -0700

    0100 frm bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -54,7 +54,7 @@ def get_bottle_plan():
                 
                 # Add the plan for bottling green potions to the list
                 plan.append({
-                    "potion_type": [0,0,100,0],  # Assuming green potions are type 0
+                    "potion_type": [0,1,0,0],  # Assuming green potions are type 0
                     "quantity": quantity
                 })
     return plan
