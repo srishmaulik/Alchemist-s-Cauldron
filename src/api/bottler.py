423f2e32ae64dcc3671be212f1189f7000d92770
srishmaulik <srishmaulik2004@gmail.com>
2024-04-10 21:28:44 -0700

    grren

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -41,7 +41,7 @@ def get_bottle_plan():
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
-    # Initial logic: bottle all barrels into red potions.
+    # Initial logic: bottle all barrels into green potions.
     for row in result:
             num_green_ml = row[0]
 
