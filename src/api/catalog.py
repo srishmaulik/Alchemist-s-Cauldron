396f1b4505f4d140fcb3770dffa36759cf3aeda9
srishmaulik <srishmaulik2004@gmail.com>
2024-04-14 01:10:43 -0700

    catalog skeleton of potions being returned

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 import sqlalchemy
 from src import database as db
 router = APIRouter()
+from sqlalchemy import select
 
 
 @router.get("/catalog/", tags=["catalog"])
@@ -10,14 +11,29 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory"))
-    available_potions = result.fetchone()[0]
+        result1 = "SELECT num_green_potions FROM global_inventory"
+        green_result = connection.execute(sqlalchemy.text(result1))
+        result2 = "SELECT num_red_potions FROM global_inventory"
+        red_result = connection.execute(sqlalchemy.text(result2))
+        result3 = "SELECT num_blue_potions FROM global_inventory"
+        blue_result = connection.execute(sqlalchemy.text(result3))
+
     return [
             {
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": min(available_potions, 10),
+                "quantity": green_result,
                 "price": 100,
-                "potion_type": [0, 100, 0, 0],
+                "potion_type": [0, 100, 0, 0]
             }
-        ]
+        ],[{ "sku": "RED_POTION_0",
+                "name": "red potion",
+                "quantity": red_result,
+                "price": 100,
+                "potion_type": [100, 0, 0, 0]}
+                ],[{ "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": blue_result,
+                "price": 100,
+                "potion_type": [0, 0, 100, 0]}]
+        
