3de6e1b9cd40197e7ccb453e512a8b8817944aec
srishmaulik <srishmaulik2004@gmail.com>
2024-04-26 12:41:33 -0700

    removed some potion types

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -46,22 +46,22 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
                 item_sku = "Blue_berry"
             elif potion.potion_type == [50,50,0,0]:
                 item_sku = "Brownie"
-            elif potion.potion_type == [60,40,0,0]:
-                item_sku = "Brownie_60"
-            elif potion.potion_type == [40,60,0,0]:
-                item_sku = "Brownie_40"
-            elif potion.potion_type == [50,0,50,0]:
-                item_sku = "Purplie"
-            elif potion.potion_type == [60,0,40,0]:
-                item_sku = "Purplie_60"
-            elif potion.potion_type == [40,0,60,0]:
-                item_sku = "Purplie_40"
-            elif potion.potion_type == [0,50,50,0]:
-                item_sku = "Yellow_mellow"
-            elif potion.potion_type == [0,40,60,0]:
-                item_sku = "Yellow_mellow_60"
-            elif potion.potion_type == [0,60,40,0]:
-                item_sku = "Yellow_mellow_40"
+            # elif potion.potion_type == [60,40,0,0]:
+            #     item_sku = "Brownie_60"
+            # elif potion.potion_type == [40,60,0,0]:
+            #     item_sku = "Brownie_40"
+            # elif potion.potion_type == [50,0,50,0]:
+            #     item_sku = "Purplie"
+            # elif potion.potion_type == [60,0,40,0]:
+            #     item_sku = "Purplie_60"
+            # elif potion.potion_type == [40,0,60,0]:
+            #     item_sku = "Purplie_40"
+            # elif potion.potion_type == [0,50,50,0]:
+            #     item_sku = "Yellow_mellow"
+            # elif potion.potion_type == [0,40,60,0]:
+            #     item_sku = "Yellow_mellow_60"
+            # elif potion.potion_type == [0,60,40,0]:
+            #     item_sku = "Yellow_mellow_40"
             elif potion.potion_type == [34,33,33,0]:
                 item_sku = "Treble"
             
@@ -126,30 +126,30 @@ def get_bottle_plan():
             elif num_red_ml==0 and num_green_ml==0 and num_blue_ml>=100:
                 plan.append({"potion_type": [0, 0, 100, 0], "quantity": 1})
                 num_blue_ml-=100
-            elif num_red_ml == 0 and num_green_ml>=40 and num_green_ml<50 and num_blue_ml>=60:
-                plan.append({"potion_type": [0, 40, 60, 0], "quantity": 1})
-                num_green_ml-=40
-                num_blue_ml-=60
-            elif num_red_ml == 0 and num_blue_ml>=40 and num_blue_ml<50 and num_green_ml>=60:
-                plan.append({"potion_type": [0, 60, 40, 0], "quantity": 1})
-                num_green_ml-60
-                num_blue_ml-=40
-            elif num_red_ml >= 60 and num_green_ml>=40 and num_green_ml<50 and num_blue_ml==0:
-                plan.append({"potion_type": [60, 40, 0, 0], "quantity": 1})
-                num_green_ml-=40
-                num_red_ml-=60
-            elif num_green_ml >= 60 and num_red_ml>=40 and num_red_ml<50 and num_blue_ml==0:
-                plan.append({"potion_type": [40, 60, 0, 0], "quantity": 1})
-                num_red_ml-=40
-                num_green_ml-=60
-            elif num_red_ml >= 60 and num_green_ml==0 and num_blue_ml<50 and num_blue_ml>=40:
-                plan.append({"potion_type": [60, 0, 40, 0], "quantity": 1})
-                num_blue_ml-=40
-                num_red_ml-=60           
-            elif num_blue_ml >= 60 and num_green_ml==0 and num_red_ml<50 and num_red_ml>=40:
-                plan.append({"potion_type": [40, 0, 60, 0], "quantity": 1})
-                num_red_ml-=40
-                num_blue_ml-=60
+            # elif num_red_ml == 0 and num_green_ml>=40 and num_green_ml<50 and num_blue_ml>=60:
+            #     plan.append({"potion_type": [0, 40, 60, 0], "quantity": 1})
+            #     num_green_ml-=40
+            #     num_blue_ml-=60
+            # elif num_red_ml == 0 and num_blue_ml>=40 and num_blue_ml<50 and num_green_ml>=60:
+            #     plan.append({"potion_type": [0, 60, 40, 0], "quantity": 1})
+            #     num_green_ml-60
+            #     num_blue_ml-=40
+            # elif num_red_ml >= 60 and num_green_ml>=40 and num_green_ml<50 and num_blue_ml==0:
+            #     plan.append({"potion_type": [60, 40, 0, 0], "quantity": 1})
+            #     num_green_ml-=40
+            #     num_red_ml-=60
+            # elif num_green_ml >= 60 and num_red_ml>=40 and num_red_ml<50 and num_blue_ml==0:
+            #     plan.append({"potion_type": [40, 60, 0, 0], "quantity": 1})
+            #     num_red_ml-=40
+            #     num_green_ml-=60
+            # elif num_red_ml >= 60 and num_green_ml==0 and num_blue_ml<50 and num_blue_ml>=40:
+            #     plan.append({"potion_type": [60, 0, 40, 0], "quantity": 1})
+            #     num_blue_ml-=40
+            #     num_red_ml-=60           
+            # elif num_blue_ml >= 60 and num_green_ml==0 and num_red_ml<50 and num_red_ml>=40:
+            #     plan.append({"potion_type": [40, 0, 60, 0], "quantity": 1})
+            #     num_red_ml-=40
+            #     num_blue_ml-=60
             
         print(plan)
         return plan
