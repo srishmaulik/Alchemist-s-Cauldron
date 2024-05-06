cbf4897b288da1dfd3795cdda30e7359e1722117
srishmaulik <srishmaulik2004@gmail.com>
2024-05-06 01:49:03 -0700

    have the highest quantity potions sell

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,6 +14,8 @@ def get_catalog():
     Each unique item combination must have only a single price.
     """
     catalog = []
+    potions_to_sell={}
+
     with db.engine.begin() as connection:
         # Query to sum the quantity for each potion type
         potion_quantities_query = """
@@ -21,34 +23,32 @@ def get_catalog():
         FROM potion_ledger_entries
         GROUP BY potion_id
         """
+        potion_quantities_result = connection.execute(sqlalchemy.text(potion_quantities_query)).fetchall()
 
-        # Fetch the results
-        potion_quantities = connection.execute(sqlalchemy.text(potion_quantities_query)).fetchall()
+        # Sort potion quantities in descending order
+        sorted_potion_quantities = sorted(potion_quantities_result, key=lambda x: x[1], reverse=True)
 
-        
-        for potion_quantity in potion_quantities:
+        # Retrieve details for the top 6 potions
+        for potion_quantity in sorted_potion_quantities[:6]:
             potion_id, total_quantity = potion_quantity
 
             # Check if the total quantity is greater than 0
             if total_quantity > 0:
                 # Retrieve potion details based on potion_id
                 potion_details_query = sqlalchemy.text(
-                    "SELECT item_sku, price, red_ml, green_ml, blue_ml FROM potions WHERE potion_id = :potion_id"
+                    "SELECT item_sku, price, red_ml, green_ml, blue_ml, dark_ml FROM potions WHERE potion_id = :potion_id"
                 )
                 potion_details = connection.execute(potion_details_query, {"potion_id": potion_id}).fetchone()
 
                 # Append potion to catalog
                 if potion_details:
-                    item_sku, price, red_ml, green_ml, blue_ml = potion_details
+                    item_sku, price, red_ml, green_ml, blue_ml, dark_ml = potion_details
                     catalog.append({
                         "sku": item_sku,
                         "name": item_sku,
                         "quantity": total_quantity,
                         "price": price,
-                        "potion_type": [red_ml, green_ml, blue_ml, 0]
+                        "potion_type": [red_ml, green_ml, blue_ml, dark_ml]
                     })
-                    if len(catalog)==6:
-                        break
-
     return catalog
-       
+            
